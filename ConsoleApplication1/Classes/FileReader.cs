01093dc5768a9c0fd96d0d77ac64b6fb80c61260
Feeling Sicknezz <80667130+feelingsicknezz@users.noreply.github.com>
2023-04-02 21:22:04 +0300

    Ну там тыры-пыры класс сделали чтобы читать файлы в общем

---
+++ ConsoleApplication1/Classes/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApplication1.Classes
+{
+    public static class FileReader
+    {
+        public static IEnumerable<string> ReadElementsFromFile(string filePath)
+        {
+            var streamReader = new StreamReader(filePath);
+
+            var text = streamReader.ReadToEnd();
+
+            var objectArray = text.Split(' ');
+            
+            return objectArray;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleApplication1.Classes;
+
+namespace ConsoleApplication1
+{
+    internal static class Program
+    {
+        private const string FilePath = "Array.txt";
+        
+        public static void Main(string[] args)
+        {
+            var array = FileReader.ReadElementsFromFile(FilePath);
+
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
